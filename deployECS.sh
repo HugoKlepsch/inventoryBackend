a1bf59f31c6ea0207aea246ff26153bbe54cefc4
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-07-06 19:05:05 -0400

    Fix wrong echo

--- deployECS.sh
+++ deployECS.sh
@@ -4,7 +4,7 @@ echo "Tagging server"
 docker tag inventoryserver:latest 938957839258.dkr.ecr.us-east-2.amazonaws.com/inventoryserver:latest
 echo "done"
 
-echo "Pushing server to ECR"
+echo "Getting ECS login info"
 $(aws ecr get-login --no-include-email --region us-east-2)
 echo "done"
 
