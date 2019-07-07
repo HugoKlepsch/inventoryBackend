c2ea5cfea8165188761b14b59206651da50f1078
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-07-07 12:02:30 -0400

    Add connect timeout to server run script
    * Also ignore some terraform files

--- server/run.sh
+++ server/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+set -x
+
 echo "Running..."
 
 echo "Waiting for database to be ready"
+env
 CONTINUE=1
 while [ "$CONTINUE" -ne 0 ]; do
   sleep 1
-  PGPASSWORD=$DBPASS psql -h $DBHOST --username=root -c "\d" inventorydb
+  PGCONNECT_TIMEOUT=6 PGPASSWORD=$DBPASS psql -h $DBHOST --username=root -c "\d" inventorydb
   CONTINUE=$?
+  echo "waiting..."
 done
 echo "Database ready"
 
