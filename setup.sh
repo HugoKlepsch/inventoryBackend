224a8ebb6141d3cb276cce9083ebf8959733c0d8
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-07-13 14:10:57 -0400

    [INV-35] Install vue cli tools in setup.sh and use node 8.11

--- setup.sh
+++ setup.sh
@@ -4,6 +4,12 @@ export BASE_DIR=$(pwd)
 
 ./server/sassCheck.sh
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+nvm install 8.11.4
+
+npm install -g @vue/cli
+
 python3 -m venv venv --system-site-packages
 
 . venv/bin/activate
