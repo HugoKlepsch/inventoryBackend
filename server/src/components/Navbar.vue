644b9ed0b84bf3a374470d647f7d2e37f32671ae
Nicholas Skoretz <nskoretz@pm.me>
2019-07-14 12:25:05 -0400

    [INV-37] Included server/src/components/*

---
+++ server/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="navbar">
+    <nav class="gnav nav">
+      <img src="/img/logo.png" alt="Logo" class="logo gnav-logo">
+      <h2 class="title gnav-title">Inventory</h2>
+      <ul class="gnav-actions">
+        <template v-for="action in actions">
+          <li>
+            <router-link v-bind:to="action.link">{{ action.title }}</router-link>
+          </li>
+        </template>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Navbar extends Vue {
+  @Prop() private actions!: array;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
