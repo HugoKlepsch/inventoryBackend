d5d4e329e269cba56309fa8bb4adfb82da6e2726
Nicholas Skoretz <nskoretz@pm.me>
2019-07-14 17:11:10 -0400

    [INV-40] Fix login page

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from './views/Login.vue';
+import Signup from './views/Signup.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'login',
       component: Login,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: Signup,
     },
     {
       path: '/about',
