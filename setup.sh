784ee52bbb4ab3b4b1c8d1d4fb661e9256d5e7ae
Hugo Klepsch <hugo.klepsch@gmail.com>
2019-07-14 11:43:46 -0400

    [INV-48] Install pip requirements from requirements.txt

--- setup.sh
+++ setup.sh
@@ -14,9 +14,9 @@ nvm install 8.11.4
 
 npm install -g @vue/cli
 
-python3 -m venv venv --system-site-packages
+python3 -m venv venv
 
 . venv/bin/activate
 
-pip install Flask requests flask_sqlalchemy psycopg2
+pip install -r server/requirements.txt
 
