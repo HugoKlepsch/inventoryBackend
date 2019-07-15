be5df71a5563cdd530c92161c9d8e5791e64b4b6
Nicholas Skoretz <nskoretz@pm.me>
2019-07-14 22:14:16 -0400

    [INV-40] Created a 'main' page in Vue, /home
    - Commented out a route for /signup that was causing errors
    - Added route to /home and Home.vue
    - Created Home.vue that is the Vue version of "main.html"
    - Edited table style for title alignment

--- server/src/router.ts
+++ server/src/router.ts
@@ -2,28 +2,30 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Login from './views/Login.vue';
 import Signup from './views/Signup.vue';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: Login,
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: Signup,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    },
-  ],
+    routes: [
+        {
+            path: '/',
+            name: 'login',
+            component: Login,
+        },
+        // {
+        //   path: '/signup',
+        //   name: 'signup',
+        //   component: Signup,
+        // },
+        {
+            path: '/home',
+            name: 'home',
+            component: Home,
+        },
+    ],
 });
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
+// component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
---
+++ server/src/views/Home.vue
@@ -0,0 +1,61 @@
+<!--
+  2019-07-14 ~~ Waiting for the API to catch up so that we can send a request to /getSales or something and retrueve a list of items to display in the table.
+  This page is navigable on the Node.js dev server at the url http://localhost:8080/#/home .
+-->
+<template>
+  <div>
+    <Navbar v-bind:actions="linkActions"/>
+    <p>This is the main user page!</p>
+    <section class="grid">
+      <div class="block-sales flex-vert">
+        <strong>Sales this Month</strong>
+        <table>
+          <tr>
+            <th v-for="header in salesTable.headers">{{ header }}</th>
+          </tr>
+          <template v-for="itemObj in salesTable.defaultItems">
+            <tr>
+              <td>{{ itemObj.item }}</td>
+              <td>{{ itemObj.buyPrice }}</td>
+              <td>{{ itemObj.sellPrice }}</td>
+            </tr>
+          </template>
+        </table>
+      </div>
+    </section>
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Navbar from '@/components/Navbar.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Navbar,
+  },
+  data: () => { // {{{
+    return {
+      linkActions: [
+        {
+          title: 'Log Out',
+          link: '/logout',
+        },
+      ],
+      salesTable: {
+        headers: [ 'Item', 'Buy Price', 'Sell Price' ],
+        defaultItems: [
+          { item: 'Thing1', buyPrice: '$100', sellPrice: '$120' },
+          { item: 'Anotha One', buyPrice: '$6.99', sellPrice: '$101.50' },
+        ],
+      },
+    };
+  }, // }}}
+})
+export default class Home extends Vue {}
+</script>
+
+
+<style lang="scss">
+</style>
